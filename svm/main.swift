c2ebdf3fdb93549ed1ce9f1b50166739977d2a89
Neil Pankey <neil@wire.im>
2014-07-07 21:20:51 -0700

    Initial commit

---
+++ svm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  svm
+//
+//  Created by Neil Pankey on 7/7/14.
+//  Copyright (c) 2014 Neil Pankey. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
