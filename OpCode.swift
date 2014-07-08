f6b49122f658acae302a37a316e42dcc4e262a3e
Neil Pankey <neil@wire.im>
2014-07-07 23:29:14 -0700

    Add remaining opcodes

--- OpCode.swift
+++ OpCode.swift
@@ -8,6 +8,39 @@
 
 // List of opcodes supported by our VM
 
-let ICONST  = 1
-let PRINT   = 2
-let HALT    = 3
+// pop 2 operands, operate, push result
+// note - second operand at top of stack, first is below it
+let IADD    = 1 // add
+let ISUB    = 2 // subtract
+let IMUL    = 3 // multiply
+let ILT     = 4 // less than
+let IEQ     = 5 // equal
+
+// brances
+let BR      = 6 // branch <addr> always
+let BRT     = 7 // pop operand, branch <addr> if operand true
+let BRF     = 8 // pop operand, branch <addr> if operand false
+
+// push a constant
+let ICONST  = 9
+
+// load
+let LOAD    = 10 // LOAD <offset> from frame pointer relative
+let GLOAD   = 11 // GLOAD <addr> from data memory
+
+// store
+let STORE   = 12 // STORE <offset> to frame pointer relative
+let GSTORE  = 13 // GSTORE <addr> to data memory
+
+// pop operand and print it to stdout
+let PRINT   = 14
+
+// discard top operand
+let POP     = 15
+
+// stop
+let HALT    = 16
+
+// TODO functions
+// call
+// ret
